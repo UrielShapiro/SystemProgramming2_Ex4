dcd3c2379c74a35e2a95065b4f623a6577665d81
Uriel Shapiro <uriel1999@gmail.com>
2024-06-22 02:27:54 +0300

    changed the children vector to store pointers

--- Node.hpp
+++ Node.hpp
@@ -1,27 +1,44 @@
 #pragma once
 #include <vector>
+#include <cmath>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using std::vector;
 
 template <typename T>
 class Node
 {
 private:
     T data;
-    std::vector<Node<T>> children;
+    vector<Node<T> *> children;
 
 public:
     Node(T data) : data(data), children{} {};
+    Node(const Node<T> &other) : data(other.get_data()), children(other.get_children()) {}
+
     void add_child(Node<T> &child)
     {
-        this->children.push_back(child);
+        this->children.push_back(&child);
     }
 
-    size_t get_num_of_childs()
+    size_t get_num_of_childs() const
     {
         return this->children.size();
     }
 
-    T get_value()
+    const T &get_data() const
     {
         return this->data;
+    }
+
+    vector<Node<T> *> get_children() const
+    {
+        return this->children;
+    }
+
+    bool is_leaf() const
+    {
+        return this->children.empty();
     }
 };
