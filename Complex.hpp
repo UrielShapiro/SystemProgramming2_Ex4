c2f802d202773a4771ef2a7036cf9695e7618a16
Uriel Shapiro <uriel1999@gmail.com>
2024-06-27 20:03:14 +0300

    Added the ability to have multiple types of numeric data members in the complex class

--- Complex.hpp
+++ Complex.hpp
@@ -1,21 +1,65 @@
+#pragma once
 #include <string>
+#include <cassert>
 
-using std::string;
+using std::string, std::to_string;
 
+template <typename T>
+constexpr bool is_numeric = false;
+template <>
+constexpr bool is_numeric<int> = true;
+template <>
+constexpr bool is_numeric<long> = true;
+template <>
+constexpr bool is_numeric<float> = true;
+template <>
+constexpr bool is_numeric<double> = true;
+
+template <typename T>
 class Complex
 {
 private:
     // Assuming a complex number is of the form: a+bi
-    float a;
-    float b;
+    T a;
+    T b;
 
 public:
-    Complex(float real, float imaginary);
-    string get_data();
+    Complex(T real, T imaginary) : a(real), b(imaginary)
+    {
+        static_assert(is_numeric<T>, "T must be a numeric type");   // Will fail at compile time if T is not a numeric type
+    }
 
-    float getReal() const;
-    float getImaginary() const;
+    string get_data() const
+    {
+        return to_string(a) + " + " + to_string(b) + "i";
+    }
 
-    bool operator==(const Complex &other) const;
-    bool operator!=(const Complex &other) const;
+    T getReal() const
+    {
+        return this->a;
+    }
+
+    T getImaginary() const
+    {
+        return this->b;
+    }
+
+    bool operator==(const Complex &other) const
+    {
+        return this->getReal() == other.getReal() && this->getImaginary() == other.getImaginary();
+    }
+    bool operator!=(const Complex &other) const
+    {
+        return !(*this == other);
+    }
+    friend bool operator>(const Complex &me, const Complex &other)
+    {
+        return sqrt(pow(me.getReal(), 2) + pow(me.getImaginary(), 2)) > sqrt(pow(other.getReal(), 2) + pow(other.getImaginary(), 2));
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const Complex<T> &complex)
+    {
+        os << complex.get_data();
+        return os;
+    }
 };
